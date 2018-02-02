9b6dc415375ac4686036a471505574c9b386312c
Omar <omar2535@gmail.com>
2018-02-01 22:24:26 -0800

    Added changing navbar colors based on current page

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const hbs = require('hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view enginer', 'hbs');
 
+hbs.registerHelper('ifCond', function(v1, v2, options) {
+    if(v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+
 const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log("app listening on port: "+ PORT);
@@ -22,5 +29,31 @@ app.get('/', (req, res)=>{
     res.render('home.hbs',{
         pageTitle: "Home",
         getCurrentYear: year=new Date().getFullYear(),
+        active: "home",
+    });
+});
+
+//Getting MyAccountPage
+app.get('/MyAccount', (req, res)=>{
+    res.render('myAccount.hbs', {
+        pageTitle: "MyAccount",
+        getCurrentYear: year=new Date().getFullYear(),
+        active: "myAccount",
+    });
+});
+
+app.get('/help', (req, res)=>{
+    res.render('help.hbs', {
+        pageTitle: "Help",
+        getCurrentYear: year=new Date().getFullYear(),
+        active: "help",
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs', {
+        pageTitle: "About",
+        getCurrentYear: year=new Date().getFullYear(),
+        active: "about",
     });
 });
