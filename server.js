b226aed69db0db665abc46486b5029a10f61d131
Omar <omar2535@gmail.com>
2018-02-10 22:05:32 -0800

    Added passport into project

--- server.js
+++ server.js
@@ -9,17 +9,39 @@ const path = require('path');                          //Path - inside NODE
 const hbs = require('hbs');                            //Handlebars 
 const passport = require('passport');                  //passportjs
 const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;  //passportjs Google strategy
+const keys = require('./config/keys');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-require('./routes')(app);                             //Initialize the routes this app will do
-require('./oauthroutes')(app, passport, GoogleStrategy);
+//Initialize the routes this app will do
+require('./routes')(app);                             
+const authRoutes = require('./routes/oauthroutes'); 
+
+//Initialize handlebars 
+app.set('view enginer', 'hbs');
+
+//To use CSS files:
+app.use(express.static(path.join(__dirname + '/public')));
+
+//Initialize session cookies 
+app.use(session({
+  secret: keys.session.cookieKey,
+  resave :false,
+  saveUninitialized: true,
+}));
+
 
 //Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Initialize Mongoose to connect to DB using URI in keys
+mongoose.connect(keys.mongodb.dbURI, ()=>{
+    console.log('connected to monbodb');
+});
+
 //Setting the pathnames and middleware
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('view enginer', 'hbs');
 
 hbs.registerHelper('ifCond', function(v1, v2, options) {
     if(v1 === v2) {
@@ -34,6 +56,5 @@ app.listen(PORT, ()=>{
     console.log("app listening on port: "+ PORT);
 });
 
-//To use CSS files:
-app.use(express.static(path.join(__dirname + '/public')));
+
 
