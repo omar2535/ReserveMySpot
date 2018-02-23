fc500914b407411b89f0869189e29f791593a9c9
Omar <omar2535@gmail.com>
2018-02-23 12:29:55 -0800

    status-check on admin page

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -5,32 +5,29 @@ This file is for admin routes
 */
 const adminRouter = require('express').Router();
 const bodyParser = require('body-parser'); //To parse URL information
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: false
+});
 const Reservation = require('../models/reservation-models');
 const adminUtils = require('../utils/admin-utils');
-const url = require('url');    
+const url = require('url');
 
 //variable for date
 var year = new Date().getFullYear();
 
 //Route for homepage on adminpage
-adminRouter.get('/', (req, res)=>{
-    Reservation.find({
-
-    }).then((reservations) => {
-    
+adminRouter.get('/', (req, res) => {
+    Reservation.find().then((reservations) => {
         adminUtils.renderIfAdmin(req, res, 'admin-panel.hbs', {
             getCurrentYear: year,
             Reservations: reservations,
-        });    
-        
+            status: req.query.status,
+        });
     });
-
-
 });
 
 //Route to get form data from admin panel
-adminRouter.post('/', urlEncodedParser, (req, res)=>{
+adminRouter.post('/', urlEncodedParser, (req, res) => {
     Reservation.findOne({
         date: req.body.date_field,
         time: req.body.time_field,
@@ -40,12 +37,7 @@ adminRouter.post('/', urlEncodedParser, (req, res)=>{
             console.log("already exists",
                 currenReservation.date, currenReservation.time, currenReservation.location);
             var status = encodeURIComponent('failed');
-            res.redirect(url.format({
-                pathname: "/admin/",
-                query: {
-                    "status": status,   
-                }
-            }));
+            res.redirect('/admin/?status=' + status);
         } else {
             //To store date as year-month-day format
             var str = req.body.date_field.toString();
@@ -61,12 +53,8 @@ adminRouter.post('/', urlEncodedParser, (req, res)=>{
                 time: req.body.time_field,
             }).save().then((newReservation) => {
                 console.log("created new reservation: ", newReservation);
-                res.redirect(url.format({
-                    pathname: "/admin/",
-                    query: {
-                        "status": 'success',   
-                    }
-                }));
+                var status = encodeURIComponent('success');
+                res.redirect('/admin/?status=' + status);
             });
         }
     });
@@ -74,15 +62,14 @@ adminRouter.post('/', urlEncodedParser, (req, res)=>{
 
 
 //Route for settings of site-settings
-adminRouter.get('/settings', (req, res)=>{
+adminRouter.get('/settings', (req, res) => {
 
 });
 
 //Route to add new schedules available for reservation
-adminRouter.get('/add', (req, res)=>{
+adminRouter.get('/add', (req, res) => {
 
 });
 
 
 module.exports = adminRouter;
-
