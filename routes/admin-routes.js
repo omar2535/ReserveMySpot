284fc44e134b5d899603fa2836c434393664a2a9
Omar <omar2535@gmail.com>
2018-02-23 23:36:36 -0800

    Implemented delete functionality
    Delete functionality in admin panel

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -28,47 +28,60 @@ adminRouter.get("/", (req, res) => {
 
 //Route to get form data from admin panel
 adminRouter.post("/", urlEncodedParser, (req, res) => {
-  Reservation.findOne({
-    date: req.body.date_field,
-    time: req.body.time_field,
-    location: req.body.location_field
-  }).then(currentReservation => {
-    if (currentReservation) {
-      console.log(
-        "already exists",
-        currentReservation.date,
-        currentReservation.time,
-        currentReservation.location
-      );
-      var status = encodeURIComponent("failed");
+  //If it was a creation submission
+  if(req.body.date_field && req.body.time_field && req.body.location_field){
+    Reservation.findOne({
+      date: req.body.date_field,
+      time: req.body.time_field,
+      location: req.body.location_field
+    }).then(currentReservation => {
+      if (currentReservation) {
+        console.log(
+          "already exists",
+          currentReservation.date,
+          currentReservation.time,
+          currentReservation.location
+        );
+        var status = encodeURIComponent("failed");
+        res.redirect("/admin/?status=" + status);
+      } else {
+        //To store date as year-month-day format
+        var str = req.body.date_field.toString();
+        var arr = [];
+        arr = str.split("-");
+        new Reservation({
+          googleId: null,
+          name: "empty",
+          year: arr[0],
+          month: arr[1],
+          date: arr[2],
+          location: req.body.location_field,
+          time: req.body.time_field,
+          reservationId: '_' + Math.random().toString(36).substr(2, 9),
+        })
+          .save()
+          .then(newReservation => {
+            console.log("created new reservation: ", newReservation);
+            var status = encodeURIComponent("success");
+            res.redirect("/admin/?status=" + status);
+          });
+      }
+    });
+  }
+  //If it was a delete submission
+  if(req.body.delete){
+    Reservation.findOneAndRemove({
+      _id: req.body.delete,
+    }, ()=>{
+      var status = encodeURIComponent("success");
       res.redirect("/admin/?status=" + status);
-    } else {
-      //To store date as year-month-day format
-      var str = req.body.date_field.toString();
-      var arr = [];
-      arr = str.split("-");
-      new Reservation({
-        googleId: null,
-        name: "empty",
-        year: arr[0],
-        month: arr[1],
-        date: arr[2],
-        location: req.body.location_field,
-        time: req.body.time_field,
-        reservationId: '_' + Math.random().toString(36).substr(2, 9),
-      })
-        .save()
-        .then(newReservation => {
-          console.log("created new reservation: ", newReservation);
-          var status = encodeURIComponent("success");
-          res.redirect("/admin/?status=" + status);
-        });
-    }
-  });
+    });
+  }
 });
 
+
 //Route for settings of site-settings
-adminRouter.get("/settings", (req, res) => {
+adminRouter.get("/settings", (req, res) => {  
   
 });
 
