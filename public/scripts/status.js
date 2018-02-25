e4ff70cbaa6bb0b3d3a8d6220d8599a5f12354ec
Omar <omar2535@gmail.com>
2018-02-24 17:49:35 -0800

    Removed querystring after redirecting

--- public/scripts/status.js
+++ public/scripts/status.js
@@ -1,3 +1,15 @@
 setTimeout(function(){
     $('#status-check').remove();
 }, 2000);
+
+var query = window.location.search.substring(1)
+
+// is there anything there ?
+if(query.length) {
+   // are the new history methods available ?
+   if(window.history != undefined && window.history.pushState != undefined) {
+        // if push state exists, add a new state the the history, this changes the url without reloading the page
+
+        window.history.pushState({}, document.title, window.location.pathname);
+   }
+}
